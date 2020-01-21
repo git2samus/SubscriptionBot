479c13098a4f9269ba4d1842585dda59edf9ae20
Michael Cetrulo <contact@michael-cetrulo.com>
2020-01-21 17:57:25 -0300

    connect database and init tables

--- bot.py
+++ bot.py
@@ -22,11 +22,39 @@ The bot.py commands are:
 See 'bot.py help <command>' for more information on a specific command.
 """
 import os
+import psycopg2
 from sys import exit
+from contextlib import closing
 from docopt import docopt, DocoptExit
 
-
 __version__ = '1.0.0'
+
+
+def init_db(database_url):
+    with closing(psycopg2.connect(database_url)) as conn:
+        with closing(conn.cursor()) as cur:
+            cur.execute("""
+                CREATE TABLE IF NOT EXISTS kv_store(key VARCHAR(32) PRIMARY KEY, value VARCHAR(256));
+            """)
+            conn.commit()
+
+            cur.execute("""
+                SELECT value FROM kv_store WHERE key = 'version';
+            """)
+            res = cur.fetchone()
+
+            if res is not None:
+                db_version = res[0]
+                if db_version != __version__:
+                    raise Exception(f"Version mismatch (current: {__version__}; db: {db_version})")
+            else:
+                cur.execute("""
+                    CREATE TABLE subscription(submission_id CHAR(6), user_id CHAR(6), PRIMARY KEY(submission_id, user_id));
+                    CREATE TABLE comment_count(submission_id CHAR(6), submission_date DATE, comment_count INTEGER DEFAULT 0, PRIMARY KEY(submission_id, submission_date));
+                    INSERT INTO kv_store VALUES('version', %s);
+                """, (__version__,))
+                conn.commit()
+
 
 if __name__ == '__main__':
     global_args = docopt(__doc__, version=f'Reddit SubscriptionBot v{__version__}', options_first=True)
@@ -44,6 +72,9 @@ if __name__ == '__main__':
     database_url = os.environ.get('DATABASE_URL')
     if database_url is None:
         raise DocoptExit("Missing DATABASE_URL value.\n")
+
+    # check db connectivity and setup tables
+    init_db(database_url)
 
     # continue processing args
     command, args = global_args['<command>'], global_args['<args>']
